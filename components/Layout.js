9fd2d46defa2cba1e861d72678eb6ee87c9f059e
Accadnan82 <accadnan@gmail.com>
2025-12-13 14:06:56 +0100

    Add layout component with navigation and footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,33 @@
+import Link from "next/link";
+import { APP_LOGIN, APP_SIGNUP } from "../lib/links";
+
+export default function Layout({ children }) {
+  return (
+    <div className="container">
+      <div className="nav">
+        <div className="brand">
+          <div className="logo">▦</div>
+          <div>
+            <div className="brandTitle">taxcheck</div>
+            <div className="brandSub">UAE Corporate Tax</div>
+          </div>
+        </div>
+
+        <div className="navLinks">
+          <Link className="btn" href="/">Home</Link>
+          <Link className="btn" href="/pricing">Pricing</Link>
+          <Link className="btn" href="/about">About</Link>
+          <a className="btn" href={APP_LOGIN}>Login</a>
+          <a className="btnPrimary" href={APP_SIGNUP}>Subscribe</a>
+        </div>
+      </div>
+
+      {children}
+
+      <div className="footer">
+        <div>© {new Date().getFullYear()} taxcheck</div>
+      </div>
+    </div>
+  );
+}
 
